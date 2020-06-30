2057cfecf2f1d2794e5d850298b0ab057b00fc62
Maicon Esperafico <memaicon@gmail.com>
2020-06-30 16:20:10 -0300

    Commit APP

---
+++ src/trabcovid/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabcovid;
+
+/**
+ *
+ * @author Maicon Esperafico
+ */
+public class Main {
+
+    public static void main(String args[]){
+        CovidMain covidMain = new CovidMain();
+        covidMain.setVisible(true);
+        covidMain.setResizable(false);
+    }
+    
+}
